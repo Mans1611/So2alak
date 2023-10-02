b5a26a5c548535a9e102800aca638916b3269849
Mans1611 <mans.yousef1611@gmail.com>
2023-10-03 00:55:13 +0300

    configure project backend & frontend

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+dotenv.config()
+const port =  process.env.PORT || 6000;
+
+app.listen(port,()=>{
+    console.log(process.env.PORT)
+    console.log(`http://localhost:${port}`)
+})
