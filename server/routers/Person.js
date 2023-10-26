8e03db6a00e6a293636d44defabcdb6c49aae54c
Mans1611 <mans.yousef1611@gmail.com>
2023-10-26 09:51:31 +0300

    making signin api in backend

--- server/routers/Person.js
+++ server/routers/Person.js
@@ -10,13 +10,12 @@ person.post('/signup',async(req,res)=>{
         const conn = await client.connect(); // conecting with database
         let sqlCommand = `SELECT * FROM students WHERE student_id=${student_id} OR studnet_name='${username}'`;
         const {rows} = await conn.query(sqlCommand);
-        console.log(rows)
         // check if this student_id has an account already or not.
         if(rows.length > 0)
             return res.status(400).json({msg:"This id is already exist s, try login"});
 
         const salt = await bcrypt.genSalt(parseInt(process.env.Salt));
-        const hashedPass =  await bcrypt.hash(password,salt)
+        const hashedPass =  await bcrypt.hash(password,salt) // encrypting the password.
         sqlCommand = `INSERT INTO students (student_id,studnet_name,student_level,password) VALUES($1,$2,$3,$4)`;
         conn.query(sqlCommand,[student_id,username,studnet_level,hashedPass]);
         conn.release(); // release the connection with the database
@@ -27,5 +26,22 @@ person.post('/signup',async(req,res)=>{
     }
 })
 
-
+person.post('/signin',async(req,res)=>{
+    const {student_id,password} = req.body;
+    try{
+        const con = await client.connect();
+        const sqlCommand = `SELECT * FROM students WHERE student_id=${student_id};`;
+        const {rows} = await con.query(sqlCommand); 
+        if(rows.length === 0)
+            return res.status(400).json({msg:"Invalid Id or password"});
+            
+        const checkPass = await bcrypt.compare(password,rows[0].password);
+        if (!checkPass)
+            return res.status(400).json({msg:"Invalid Email or password"});
+       
+        return res.status(200).json({msg:"Welcome"});
+    }catch(err){
+        console.log(err)
+    }
+})
 export default person;
