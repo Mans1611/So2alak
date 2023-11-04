3ce3f7db6c861bda02d904cabb1a4c62d994624d
unknown <abdullahmostafa9957@gmail.com>
2023-11-04 23:06:15 +0200

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUP from './pages/SignUP';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+
+        <div className="App">
+          <Routes>
+
+            <Route path='/signup' element={<SignUP />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
