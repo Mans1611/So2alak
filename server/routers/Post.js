579f652ca922d2ff9191789cd8dca0182c127163
Mans1611 <mans.yousef1611@gmail.com>
2023-11-06 13:13:16 +0200

    making search case sensitive

--- server/routers/Post.js
+++ server/routers/Post.js
@@ -6,8 +6,6 @@ import client from "../databse.js";
 const post = Router();
 /* 
 1 - middleware for authntication 
-2 - Check letter case sensitivity
-
 */
 
 post.get('/searchcourse/:searchString',async(req,res)=>{
@@ -15,8 +13,8 @@ post.get('/searchcourse/:searchString',async(req,res)=>{
     try {
         const con = await client.connect();
         let sqlCommand = `SELECT * FROM courses 
-        WHERE course_name LIKE '%${searchString}%' 
-        OR course_id LIKE '%${searchString}%';`
+        WHERE course_name ILIKE '%${searchString}%' 
+        OR course_id ILIKE '%${searchString}%';`
         const {rows} = await con.query(sqlCommand);
         return res.status(200).json({courses:rows})
     } catch (error) {
