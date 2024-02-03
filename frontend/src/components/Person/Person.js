9cb85197dd89cf97e7ccf4a2ce7cc39086902b87
Mans1611 <mans.yousef1611@gmail.com>
2024-02-04 00:00:04 +0200

    making person comp and modal

--- frontend/src/components/Person/Person.js
+++ frontend/src/components/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { team_members } from '../../data/Team_member_Data';
 
 const Person = (props) => {
+
   return (
-    <div className="person">
+    <div  onClick={()=>{props.setSelected(team_members[props.id]);props.setShowModal(true)}} className="person">
         <img className='person-img' src={props.img} alt={props.name} srcset="" />
         <h1 style={{bottom:props.middlle && '20px'}} className={`person-name ${!props.middle&& 'side'}`}>{props.name}</h1>
     </div>
--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -1,29 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './about.scss';
 import abdullah from '../../assets/abdullah.png';
+
 import mans from '../../assets/mans.png';
 import omar from '../../assets/omar.png';
 import mostafa from '../../assets/mostafa.png';
 import curve from '../../assets/about_curve.svg'
 import Person from '../../components/Person/Person';
+import { team_members } from '../../data/Team_member_Data';
+
+const Modal = ({ showModal, closeModal,selected_member }) => {
+    console.log(selected_member)
+    return (
+      <div className={`modal-overlay ${showModal ? 'show' : ''}`} onClick={()=>closeModal(false)}>
+        
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="photo-wrapper">
+                <img src={selected_member.active_img}  srcset="" />
+            </div>
+            <div className="info-wrapper">
+                <h1 className='person-name'>
+                    {selected_member.name}
+                </h1>
+                <h2 className='person-title'>{selected_member.title}</h2>
+                <p className='bio'>{selected_member.bio}</p>
+            </div>
+        </div>
+      </div>
+    );
+};
+
 const About = () => {
-    document.title = 'About Us'
+    document.title = 'About Us';
+    const [showPersonModal,setShowPersonModal] = useState(false)
+    const [selected_member,setSelected] = useState({})
+    
   return (
     <div className='about-page'>
         <div className="our-photos-wrapper">
             <h1 className='team-title'>OUR TEAM</h1>
             <div className="flex">
-                <Person name="Mansour Mohamed" img ={mans}/>
-                <Person name="Mostafa Abdelal" img ={mostafa} middle={true}/>
-                <Person name="AbdAllah Mostafa" img ={abdullah} middle={true}/>
-                <Person name="Omar Rehan" img ={omar}/>
-                
+                {
+                    team_members.map((member,key)=>
+                    <Person name = {member.name} 
+                            img = {member.img} 
+                            middle = {member.middle}
+                            id={key}
+                            setShowModal = {setShowPersonModal} 
+                            setSelected = {setSelected} 
+                            />)
+                }
             </div>
             <img src={curve} className='curve' alt="" srcset="" />
         </div>
         <div className="details-container">
             
         </div>
+        <Modal showModal={showPersonModal} selected_member ={selected_member} closeModal={setShowPersonModal} />
     </div>
   )
 }
