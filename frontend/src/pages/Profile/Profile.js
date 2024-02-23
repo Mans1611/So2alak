b97e6bd7afdfc10dc13fc926a74ab3fb4b7f00ed
عبدالله مصطفى <abdullahmostafa9957@gmail.com>
2024-02-23 09:24:47 +0200

    Update Profile.js

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
-import './Profile.scss'
+import React, { useState } from 'react';
+import './Profile.scss';
+import avatar from "../../assets/user-tie-solid.svg";
 
 // انا حطتلك الملف دا في ملف ال app.js
 // رن الفرونت اند و روح على اللينك دا 
 //  /main/profile/
 const Profile = () => {
+    const [username, setUserame] = useState("Mans116");
+    const [description, setDescription] = useState("Senior CSE Student @ ASU");
+    
     // طب ماهو فيه شهور 31 يوم و فيه شهرنا دا فيه 29 كل اربع سنسن
     const [months, setMonths] = useState(Array(12).fill(Array(30).fill(0))); 
     const months_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-    
     const [points, setPoints] = useState(0);
     const [questions, setQuestions] = useState(0);
     const [answers, setAnswers] = useState(0);
@@ -27,6 +30,13 @@ const Profile = () => {
                 </div>
                 {/*  دا بس اللي انا عايزه في ملف البروفايل غير كدا هو عندي. يعني اللي فوق دا يتشال */}
                 <div className='info'>
+                    <div className="preUpper">
+                        <img src={avatar} alt="" />
+                        <div className="discription">
+                            <p>{username}</p>
+                            <p>{description}</p>
+                        </div>
+                    </div>
                     <div className='upper'>
                         {/* دا كومبوننت لوحده اسمه سكور عشان بعد كدا هنعمل بيه فيتش 
                         ويكون جواه الستيات بتاعته زي البوينت و الاسئله و كدا.
