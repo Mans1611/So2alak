299cb627a3f3342fe97eff6bb44518783b88ad98
عبدالله مصطفى <abdullahmostafa9957@gmail.com>
2024-02-23 09:30:26 +0200

    Update Profile.js for responsive

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
-import './Profile.scss';
+import React, { useState } from 'react'
+import './Profile.scss'
 import avatar from "../../assets/user-tie-solid.svg";
 
-// انا حطتلك الملف دا في ملف ال app.js
-// رن الفرونت اند و روح على اللينك دا 
-//  /main/profile/
 const Profile = () => {
     const [username, setUserame] = useState("Mans116");
     const [description, setDescription] = useState("Senior CSE Student @ ASU");
-    
-    // طب ماهو فيه شهور 31 يوم و فيه شهرنا دا فيه 29 كل اربع سنسن
-    const [months, setMonths] = useState(Array(12).fill(Array(30).fill(0))); 
+
+    const [months, setMonths] = useState(Array(12).fill(Array(30).fill(0)));
     const months_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
     const [points, setPoints] = useState(0);
@@ -23,12 +19,9 @@ const Profile = () => {
                 nav bar
             </div>
             <div className='content'>
-                {/* شيل السايد بار عشان هتتحط ف  */}
-
                 <div className='side_bar'>
                     side bar
                 </div>
-                {/*  دا بس اللي انا عايزه في ملف البروفايل غير كدا هو عندي. يعني اللي فوق دا يتشال */}
                 <div className='info'>
                     <div className="preUpper">
                         <img src={avatar} alt="" />
@@ -38,10 +31,6 @@ const Profile = () => {
                         </div>
                     </div>
                     <div className='upper'>
-                        {/* دا كومبوننت لوحده اسمه سكور عشان بعد كدا هنعمل بيه فيتش 
-                        ويكون جواه الستيات بتاعته زي البوينت و الاسئله و كدا.
-                        */}
-                        
                         <div className='score'>
                             <div className='score_in'>
                                 <div className='achieved'><p>{points}</p> <p>Points</p></div>
@@ -49,9 +38,6 @@ const Profile = () => {
                                 <div className='achieved'><p>{answers}</p> <p>Answers</p></div>
                             </div>
                         </div>
-                        {/*  ودا اخر الكومبوننت السكور */}
-
-                        {/* ودا برضوا كومبوننت البادجيز  */}
                         <div className='badges'>
                             <div className='badges_in'>
                                 Badges
@@ -59,7 +45,6 @@ const Profile = () => {
                         </div>
                     </div>
                     <div className='lower'>
-                        {/* ودا كومبوننت الكالندر  */}
                         <div className='lower_in'>
                             <h1>Activity Calendar</h1>
                             <div className='calendar'>
